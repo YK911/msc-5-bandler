c3e5ebb8c316a6218b7cb4ab26c9054795c92a00
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-07-20 17:24:36 +0300

    completed blended lesson 02

--- src/main.js
+++ src/main.js
@@ -10,3 +10,22 @@ const toggleModal = () => {
 
 openBtn.onclick = toggleModal;
 closeBtn.onclick = toggleModal;
+
+const mobileMuneRefs = {
+  mobileMenu: document.querySelector('.js-mobile-menu'),
+  openMenuBtn: document.querySelector('.js-open-menu'),
+  closeMenuBtn: document.querySelector('.js-close-menu'),
+};
+
+const toggleMenu = () => {
+  const isMenuOpen =
+    mobileMuneRefs.openMenuBtn.getAttribute('aria-expanded') === 'true' ||
+    false;
+
+  mobileMuneRefs.mobileMenu.classList.toggle('is-open');
+  mobileMuneRefs.mobileMenu.setAttribute('aria-hidden', isMenuOpen);
+  mobileMuneRefs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+};
+
+mobileMuneRefs.openMenuBtn.addEventListener('click', toggleMenu);
+mobileMuneRefs.closeMenuBtn.addEventListener('click', toggleMenu);
